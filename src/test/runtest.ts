fe09efb06c135b7e08e8129515039ba56c5bea14
drossi750 <drossi750@gmail.com>
2020-04-14 21:52:27 +0200

    Cleanup + licenses

--- src/test/runtest.ts
+++ src/test/runtest.ts
@@ -1,18 +1,28 @@
+// Copyright (C) 2020 Davide Rossi
+// 
+// This file is part of vscode-salesforce-toolkit.
+// 
+// vscode-salesforce-toolkit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// vscode-salesforce-toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with vscode-salesforce-toolkit.  If not, see <http://www.gnu.org/licenses/>.
+
 import * as path from 'path';
 
 import { runTests } from 'vscode-test';
 
 async function main() {
     try {
-        // The folder containing the Extension Manifest package.json
-        // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-        // The path to the extension test script
-        // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
-        // Download VS Code, unzip it and run the integration test
         await runTests({ extensionDevelopmentPath, extensionTestsPath });
     } catch (err) {
         console.error('Failed to run tests');
